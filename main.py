0dc7926ea939a067795081f44c92678d7e2166a9
Alexander Vasilevsky <vasalvit@gmail.com>
2019-02-16 19:25:11 +0300

    Add the help message

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from telegram.ext import Updater, CommandHandler
 import os
 
+# noinspection PyPackageRequirements
+from telegram.ext import Updater, CommandHandler
 
-def hello(bot, update):
+
+def command_help(_, update):
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name)
+        'Example: Send "2d6+1" to roll dices and calculate the result'
     )
 
 
 updater = Updater(os.environ['TELEGRAM_BOT_TOKEN'])
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler(['help', 'h', '?'], command_help))
 
 updater.start_polling()
 updater.idle()
