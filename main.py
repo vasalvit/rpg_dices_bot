913a8e28b8af01f9b0f9dbf282d936235a4cd444
Alexander Vasilevsky <vasalvit@gmail.com>
2019-02-17 10:27:14 +0300

    Add the MIT license, add new commands

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import os
 
 from telegram.ext import Updater, CommandHandler, RegexHandler
-from string import Template
 
 from dices import parse, calculate, InvalidFormat, InvalidDicesCount, InvalidFacesCount, minimal_dices_count, \
     maximal_dices_count, minimal_faces_count, maximal_faces_count
 
 
 def command_help(_, update):
-    update.message.reply_text(
-        'Example: Send "1d4" or "2d6+1" to roll dices and calculate the result'
-    )
+    update.message.reply_text('Example: Send "1d4" or "2d6+1" to roll dices and calculate the result')
 
 
-def roll_dices(bot, update):
+def command_license(_, update):
+    update.message.reply_text('MIT')
+
+
+def command_author(_, update):
+    update.message.reply_text('Alexander Vasilevsky')
+
+
+def command_copyright(_, update):
+    update.message.reply_text('Copyright (c) 2019 Alexander Vasilevsky')
+
+
+def command_github(_, update):
+    update.message.reply_text('https://github.com/vasalvit/rpg_dices_bot')
+
+
+def roll_dices(_, update):
     try:
         dices = parse(update.message.text)
 
@@ -38,6 +51,11 @@ def roll_dices(bot, update):
 updater = Updater(os.environ['TELEGRAM_BOT_TOKEN'])
 
 updater.dispatcher.add_handler(CommandHandler(['help', 'h', '?'], command_help))
+updater.dispatcher.add_handler(CommandHandler(['license'], command_license))
+updater.dispatcher.add_handler(CommandHandler(['author'], command_author))
+updater.dispatcher.add_handler(CommandHandler(['copyright'], command_copyright))
+updater.dispatcher.add_handler(CommandHandler(['github', 'git'], command_github))
+
 updater.dispatcher.add_handler(RegexHandler(r'.*', roll_dices))
 
 updater.start_polling()
